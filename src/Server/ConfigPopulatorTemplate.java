586c428be79bc4ce203aa152a938cdfed81d27f6
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-01-13 11:02:05 -0800

    Changed the name of ConfigPopulator to ConfigPopulatorTemplate

--- src/Server/ConfigPopulatorTemplate.java
+++ src/Server/ConfigPopulatorTemplate.java
@@ -1,10 +1,10 @@
 package Server;
 //This class is used to populate the config file for the first time with the proper information
-public class ConfigPopulator {
+public class ConfigPopulatorTemplate {
     public static void main(String[] args) throws ConfigNotInitializedException, InvalidAttributeValueException {
         Config.initializeConfig("ServerConfiguration");
-        Config.setEmailUsername("YourEmailHere");
-        Config.setEmailPassword("YourPasswordHere");
+        Config.setEmailUsername("Jstojkovic@Callutheran.edu");
+        Config.setEmailPassword("T*r1TheCat4");
         Config.setUserDatabaseServerAddress("jdbc:mysql://localhost:3306/userdb");
         Config.setDatabaseUsername("ClientServerProject");
         Config.setDatabasePassword("");
