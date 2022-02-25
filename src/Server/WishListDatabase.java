bedcca32aa6fdc585140cb4feaabd1b5eec2f16e
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-02-25 15:49:51 -0800

    Added addEntry method to WishListDatabase

--- src/Server/WishListDatabase.java
+++ src/Server/WishListDatabase.java
@@ -16,15 +16,33 @@ public class WishListDatabase extends Database{
     //method to get a users wish list
     public ArrayList<String> getWishList(User usr){
         ArrayList<String> wishList = new ArrayList<>();
+        int listId = 0;
         try {
             rset = query("select id from wishlistindex where ownerId = '" + usr.getId() + "';");
             while (rset.next()){
-                System.out.println(rset.getString(1));
+                listId = rset.getInt(1);
             }
+            rset = query("select description from wishlistentries where id = '" + listId +"';");
+                while(rset.next()){
+                    System.out.println(rset.getString(1));
+                }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return wishList;
+    }
+    //method to add an entry into a wishlist
+    public void addEntry(User usr, String entry){
+        int listId = 0;
+        try {
+            rset = query("select id from wishlistindex where ownerId = '" + usr.getId() + "';");
+            while (rset.next()) {
+                listId = rset.getInt(1);
+            }
+            update("INSERT INTO `wishlistentries` (`id`, `description`) VALUES ('" + listId + "', '" + entry + "');");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
     //main method for testing
     public static void main(String[] args) {
@@ -33,11 +51,10 @@ public class WishListDatabase extends Database{
             UserDatabase usrdb = new UserDatabase(Config.getUserDatabaseServerAddress(),Config.getDatabaseUsername(),Config.getDatabasePassword());
             WishListDatabase wlDB = new WishListDatabase(Config.getSystemDatabaseServerAddress(),Config.getDatabaseUsername(),Config.getDatabasePassword());
             User usr = usrdb.getUser("test");
+//            wlDB.addEntry(usr, "Test of the add entry method");
             wlDB.getWishList(usr);
-        } catch (ConfigNotInitializedException e) {
+        } catch (ConfigNotInitializedException | SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
     }
 }
