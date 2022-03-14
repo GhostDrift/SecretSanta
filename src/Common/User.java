f7086d3697592b311861693b01028b07aa1be0c0
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-03-14 15:59:18 -0700

    Added name field to User object, along with a getter and setter

--- src/Common/User.java
+++ src/Common/User.java
@@ -17,13 +17,14 @@ public class User implements Serializable {
     private ArrayList<String> wishList;
     private int ssrid;
     private String entry;
+    private String name;
 
     //Default constructor
     public User() {
     }
 
     //Overloaded constructors
-    public User(int id,String username, String password, String email, int lockCount, int loggedIn, int ssrid) {
+    public User(int id,String username, String password, String email, int lockCount, int loggedIn, int ssrid, String name) {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -32,12 +33,14 @@ public class User implements Serializable {
         this.id = id;
         this.wishList = new ArrayList<>();
         this.ssrid = ssrid;
+        this.name = name;
     }
 
-    public User(String username, String password, String email) {
+    public User(String username, String password, String email, String name) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.name = name;
     }
     public User(String username){
         this.username = username;
@@ -108,6 +111,12 @@ public class User implements Serializable {
     }
     public void setEntry(String entry){
         this.entry = entry;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
     }
 
     public void print(){
