5e9b766cec86db18e2795a115c88f4e271c006b7
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-03-18 10:27:40 -0700

    Created connections class in Client package

---
+++ src/Client/Connections.java
@@ -0,0 +1,8 @@
+package Client;
+
+import java.io.Serializable;
+
+//a class used to hold the information for the saved connections for the client
+public class Connections implements Serializable {
+
+}
