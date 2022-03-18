02a52d586609a44b23631607da91d977f1f114e9
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-03-18 11:06:48 -0700

    Added methods to Connections

--- src/Client/Connections.java
+++ src/Client/Connections.java
@@ -1,8 +1,103 @@
 package Client;
 
-import java.io.Serializable;
-
+import java.io.*;
+import java.util.ArrayList;
+////////////This class is a Singleton////////////
 //a class used to hold the information for the saved connections for the client
-public class Connections implements Serializable {
+class Connections implements Serializable {
+    //variables
+    private static ArrayList<String> names;
+    private static ArrayList<String> ips;
+    private static ArrayList<Integer> ports;
+    private static final String filePath = "Connections.txt";
+    private static Connections connections;
 
+    //constructor
+    protected Connections(){
+        initialize();
+    }
+    //method to initialize object
+    private void initialize(){
+        if(connections == null){
+            connections = new Connections();
+        }
+        load();
+    }
+    //method to load the connections object
+    private void load(){
+        try{
+            FileInputStream fileIn = new FileInputStream(filePath);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            connections = (Connections) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (FileNotFoundException e) {
+            assignDefaultValues();
+            saveConnections();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    //method to save the connections object
+    public static void saveConnections() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filePath);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(connections);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //method to assign the default values to the object
+    private void assignDefaultValues(){
+        names = new ArrayList<>();
+        ips = new ArrayList<>();
+        ports = new ArrayList<>();
+    }
+
+    //////////// Getters and Setters ////////////
+    protected static ArrayList<String> getNames() throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        return names;
+    }
+    protected static void setNames(ArrayList<String> newNames) throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        names = newNames;
+    }
+    protected static ArrayList<String> getIps() throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        return ips;
+    }
+    protected static void setIps(ArrayList<String> newIps) throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        ips = newIps;
+    }
+    protected static ArrayList<Integer> getPorts() throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        return ports;
+    }
+    protected static void setPorts(ArrayList<Integer> newPorts) throws ConnectionsNotInilizedException {
+        if(connections == null){
+            throw new ConnectionsNotInilizedException();
+        }
+        ports = newPorts;
+    }
+
+    private static class ConnectionsNotInilizedException extends Throwable {
+        public ConnectionsNotInilizedException(){
+            super("Connections Has not been initialized");
+        }
+    }
 }
